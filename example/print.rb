46d596ed5a9b49dedaa98412b10252da8d40ad31
hitode909 <hitode909@gmail.com>
2011-01-18 16:01:27 +0900

    bin/

--- example/print.rb
+++
@@ -1,9 +0,0 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-require 'user-stream-receiver'
-require 'json'
-
-UserStreamReceiver.new.run{|chunk|
-  p JSON.parse(chunk)
-}
